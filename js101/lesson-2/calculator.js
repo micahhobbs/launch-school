9294ee8fdc97577235555a3181b8d72172fd441f
Micah T. Hobbs <micahthobbs@gmail.com>
2023-08-11 15:28:11 +1000

    Calculator app

---
+++ js101/lesson-2/calculator.js
@@ -0,0 +1,26 @@
+const readline = require ('readline-sync');
+// import readline from 'readline-sync';
+
+console.log("Welcome to Calculator!");
+
+console.log("What's your first number?");
+let number1 = readline.question();
+
+console.log("What's your second number?");
+let number2 = readline.question();
+
+console.log("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
+let operation = readline.question();
+
+let output;
+if (operation === '1') {
+  output = Number(number1) + Number(number2);
+} else if (operation === '2') {
+  output = Number(number1) - Number(number2);
+} else if (operation === '3') {
+  output = Number(number1) * Number(number2);
+} else if (operation === '4') {
+  output = Number(number1) / Number(number2);
+}
+
+console.log(`The result is: ${output}`);
