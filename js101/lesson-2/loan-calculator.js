b8bd309d41ce6e9c0a9ed29ee65e0a531631676f
Micah T. Hobbs <micahthobbs@gmail.com>
2023-08-15 21:42:24 +1000

    V1 of loan calculator

---
+++ js101/lesson-2/loan-calculator.js
@@ -0,0 +1,24 @@
+/* eslint-disable max-len */
+const readline = require('readline-sync');
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+prompt('Welcome to Loan Calculator');
+
+prompt('How much is your loan?');
+let loanTotal = readline.question();
+
+prompt('How much is the the Annual Percentage Rate (APR)?');
+let apr = readline.question();
+
+prompt('How long is the loan in years?');
+let loanYears = readline.question();
+
+let monthlyRate = (apr / 100) / 12;
+let loanMonths = loanYears * 12;
+
+let monthlyPayment = loanTotal * (monthlyRate / (1 - Math.pow((1 + monthlyRate), (-loanMonths))));
+
+console.log(`Your monthly payment will be: $${monthlyPayment.toFixed(2)}`);
